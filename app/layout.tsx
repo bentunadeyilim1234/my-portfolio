b7d8ef3f30a98407f4b782cad1aef145e5355a16
bentunadeyilim1234 <tunaonair@gmail.com>
2024-04-08 13:53:38 +0300

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Lexend } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 const lexend = Lexend({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`h-dvh w-full flex justify-center bg-[#FFF2E1] text-white ${lexend.className}`}>
+        <Analytics/>
         <div className="max-w-2xl w-full h-full">
           {children}
         </div>
