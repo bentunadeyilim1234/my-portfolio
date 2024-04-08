f9567fd1bea3607dc8bb99b5fea4bac925c786be
bentunadeyilim1234 <tunaonair@gmail.com>
2024-04-08 13:55:26 +0300

    Update analytics positioning

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,9 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`h-dvh w-full flex justify-center bg-[#FFF2E1] text-white ${lexend.className}`}>
-        <Analytics/>
         <div className="max-w-2xl w-full h-full">
           {children}
+          <Analytics/>
         </div>
       </body>
     </html>
